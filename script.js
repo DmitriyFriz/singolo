31c45de6f1aaf187293d61ccc25505d84f262167
DmitriyFriz <dmitriy.bulavko.friz44@gmail.com>
2020-03-15 22:32:50 +0300

    feat: add get a quote script

--- script.js
+++ script.js
@@ -3,6 +3,16 @@ const portfolioGallery = document.getElementById('portfolio-gallery');
 const portfolioImages = portfolioGallery.querySelectorAll('.portfolio-img');
 const portfolioTags = document.getElementById('portfolio-tags');
 
+const submitBtn = document.getElementById('submit');
+const nameField = document.getElementById('name-field');
+const emailField = document.getElementById('email-field');
+const subjectField = document.getElementById('subject-field');
+const describeField = document.getElementById('describe-field');
+const subjectInfo = document.getElementById('subject-info');
+const describeInfo = document.getElementById('describe-info');
+const frameMessage = document.getElementById('frame-message')
+const okBtn = document.getElementById('ok-btn');
+
 // HEADER
 menu.addEventListener('click', (event) => {
   let menuElement = event.target.closest('.navigation-item');
@@ -24,4 +34,16 @@ portfolioTags.addEventListener('click', (event) =>{
   portfolioTags.querySelectorAll('.tag').forEach((item) =>item.classList.remove('tag-active'));
   event.target.classList.add('tag-active');
   portfolioImages.forEach((item) => item.style.order = `${Math.round(Math.random()*100)}`);
+})
+
+// GET A QUOTE FORM AND MESSAGE
+submitBtn.addEventListener('click', () => {
+  if (!(nameField.reportValidity() && emailField.reportValidity())) return;
+  frameMessage.classList.remove('hidden');
+  if (subjectField.value) subjectInfo.textContent = `Тема: ${subjectField.value}` ;
+  if (describeField.value) describeInfo.textContent = `Описание: ${describeField.value}` ;
+})
+
+okBtn.addEventListener('click', () => {
+  frameMessage.classList.add('hidden');
 })
